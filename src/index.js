641f63b1b257e6fb9a72d9d65b40c1b0e6b44b95
Benjamin <benmma13@gmail.com>
2022-08-19 21:04:26 +0400

    add functions for adding

--- src/index.js
+++ src/index.js
@@ -1,39 +1,49 @@
+// all imports goes here
+// import { Template } from 'webpack';
 import './styles/main.css';
-// import joke from './joke.js';
 
-// const jokes = document.querySelector('.joke');
-// jokes.innerHTML = joke();
+// reference section starts here
+const mainTodo = document.querySelector('.todo');
+const firstmat = document.getElementById('mat1');
+const input = document.querySelector('.span2');
+const clearbtn = document.querySelector('.footer');
 
-const todo = document.querySelector('.todo');
-
-const todoArr = [
-  {
-    Description: 'ironing',
-    Completed: false,
-    Index: 0,
-  },
-  {
-    Description: 'washing',
-    Completed: false,
-    Index: 1,
-  },
-  {
-    Description: 'cooking',
-    Completed: false,
-    Index: 2,
-  },
-];
-
-function showTask() {
-  for (let i = 0; i < todoArr.length; i += 1) {
-    todo.innerHTML += ` <div class="tasks">
-    <div class="task-wrapper">
-      <input class="check" type="checkbox">
-      <p class="txt">${todoArr[i].Description}</p>
-    </div>
-    <button class="remove-btn">Remove</button>
-    </div>`;
+// Template class objects starts here
+class Myobject {
+  constructor(description, completed, index){
+    this.description = description
+    this.completed = completed
+    this.index = index
   }
 }
 
-showTask();
+const myArray = [];
+const addTodo = (todoValue) => {
+  const todocontainer = document.createElement('div');
+  todocontainer.className = 'todocontainer';
+  todocontainer.innerHTML += `
+    <input type="checkbox" class="checkbox";
+    <span>${todoValue}</span>
+    <span id="add-to-icon" class="material-icons"> more_horiz</span>
+    <span id="basketdel-icon" class="material-icons">delete_forever</span>
+  `;
+
+  mainTodo.appendChild(todocontainer);
+
+  // checkbox starts here
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach(i =>{
+    i.addEventListener('click', ()=>{
+      i.parentElement.classList.toggle('checked')
+    })
+  })
+
+};
+
+input.addEventListener('keypress', e=>{
+  if (e.key === 'Enter' && input.value) {
+    e.preventDefault();
+    addTodo(input.value);
+    input.value = "";
+  }
+})
